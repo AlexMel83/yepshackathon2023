7bedc25445966d499a3d56021fbf3d1c10a238fb
AlexMel83 <meleshko.a.n@ukr.net>
2023-07-29 14:51:41 +0300

    save

--- js/component.js
+++ js/component.js
@@ -59,167 +59,78 @@ function hrefLangEN() {
     return hrefEn;
 }
 
-let navAboutUs = ` Про студію</span>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#gallery">Галерея</a></li>
-                    <li><a class="dropdown-item" href="#teachers">Викладачі</a></li>
-                    <li><a class="dropdown-item" href="#feedback">Відгуки</a></li>
-                    <li><a class="dropdown-item" href="#faq">Запитання та відповіді</a></li>`;
-let navTeaching = `Навчання</span>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="education-for-children.html">для дітей</a></li>
-                    <li><a class="dropdown-item" href="education-for-prospective-students.html">для
-                    абітурієнтів</a></li>
-                    <li><a id="adultlink" class="dropdown-item" href="education-for-adults.html">для
-                    дорослих</a></li>`;
-let navMasterClasses = `<a class="nav-link menu-link" href="master.html">Майстер-класи</a>`;
-let navContacts = `<a class="nav-link menu-link" href="#contacts">Контакти</a>`;
-let navLangSelect = `UA</span>
-                    <ul class="dropdown-menu lang">
-                    <li><a class="dropdown-item lang" href=${hrefLangUA()}>UA</a></li>
-                    <li><a class="dropdown-item lang" href=${hrefLangEN()}>EN</a></li>`;
-let navOrderLesson = "Замовити урок";
 
-// drawHeader();
+
+drawHeader();
 
 function drawHeader() {
+    let aboutus = 'Про нас', navlern = 'Навчальний розділ';
+
+
     if (!isIndex && lengthPath > 1) {
         if (langEn) {
-            navAboutUs = `About us</span>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="index-en.html#gallery">Gallery</a></li>
-                <li><a class="dropdown-item" href="index-en.html#teachers">Teachers</a></li>
-                <li><a class="dropdown-item" href="index-en.html#feedback">Reviews</a></li>
-                <li><a class="dropdown-item" href="index-en.html#faq">Questions and answers</a></li>`;
-            navTeaching = `Teaching</span>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="education-for-children-en.html">for children</a></li>
-                    <li><a class="dropdown-item" href="education-for-prospective-students-en.html">for applicants</a></li>
-                    <li><a id="adultlink" class="dropdown-item" href="education-for-adults-en.html">for adults</a></li>`;
-            navMasterClasses = `<a class="nav-link menu-link" href="master-en.html">Master classes</a>`;
-            navContacts = `<a class="nav-link menu-link" href="#contacts">Contacts</a>`;
-            navLangSelect = `EN</span>
-                            <ul class="dropdown-menu lang">
-                            <li><a class="dropdown-item lang" href=${hrefLangUA()}>UA</a></li>
-                            <li><a class="dropdown-item lang" href=${hrefLangEN()}>EN</a></li>`;
-            navOrderLesson = "Book a lesson";
-            mobLangSelect = `<a href=${hrefLangUA()}>UA</a>
-                                <div class="vertical"></div>
-                            <a class="lang-select" href=${hrefLangEN()}>EN</a>`;
-            logoLink = "index-en.html";
+
         } else {
-            navAboutUs = ` Про студію</span>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="index.html#gallery">Галерея</a></li>
-                <li><a class="dropdown-item" href="index.html#teachers">Викладачі</a></li>
-                <li><a class="dropdown-item" href="index.html#feedback">Відгуки</a></li>
-                <li><a class="dropdown-item" href="index.html#faq">Запитання та відповіді</a></li>`;
-            navTeaching = `Навчання</span>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="education-for-children.html">для дітей</a></li>
-                    <li><a class="dropdown-item" href="education-for-prospective-students.html">для
-                    абітурієнтів</a></li>
-                    <li><a id="adultlink" class="dropdown-item" href="education-for-adults.html">для
-                    дорослих</a></li>`;
-            navMasterClasses = `<a class="nav-link menu-link" href="master.html">Майстер-класи</a>`;
-            navContacts = `<a class="nav-link menu-link" href="#contacts">Контакти</a>`;
-            mobLangSelect = `<a class="lang-select" href=${hrefLangUA()}>UA</a>
-                                <div class="vertical"></div>
-                            <a href=${hrefLangEN()}>EN</a>`;
-            navOrderLesson = "Замовити урок";
+
         }
     } else {
         if (langEn) {
-            navAboutUs = `About us</span>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#gallery">Gallery</a></li>
-                <li><a class="dropdown-item" href="#teachers">Teachers</a></li>
-                <li><a class="dropdown-item" href="#feedback">Reviews</a></li>
-                <li><a class="dropdown-item" href="#faq">Questions and answers</a></li>`;
-            navTeaching = `Teaching</span>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="education-for-children-en.html">for children</a></li>
-                <li><a class="dropdown-item" href="education-for-prospective-students-en.html">for applicants</a></li>
-                <li><a id="adultlink" class="dropdown-item" href="education-for-adults-en.html">for adults</a></li>`;
-            navMasterClasses = `<a class="nav-link menu-link" href="master-en.html">Master classes</a>`;
-            navContacts = `<a class="nav-link menu-link" href="#contacts">Contacts</a>`;
-            navLangSelect = `EN</span>
-                        <ul class="dropdown-menu lang">
-                        <li><a class="dropdown-item lang" href=${hrefLangUA()}>UA</a></li>
-                        <li><a class="dropdown-item lang" href=${hrefLangEN()}>EN</a></li>`;
-            navOrderLesson = "Book a lesson";
-            mobLangSelect = `<a href=${hrefLangUA()}>UA</a>
-                                <div class="vertical"></div>
-                            <a class="lang-select" href=${hrefLangEN()}>EN</a>`;
-            logoLink = "index-en.html";
+
         } else {
-            navAboutUs = ` Про студію</span>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#gallery">Галерея</a></li>
-                                <li><a class="dropdown-item" href="#teachers">Викладачі</a></li>
-                                <li><a class="dropdown-item" href="#feedback">Відгуки</a></li>
-                                <li><a class="dropdown-item" href="#faq">Запитання та відповіді</a></li>`;
-            navTeaching = `Навчання</span>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="education-for-children.html">для дітей</a></li>
-                                    <li><a class="dropdown-item" href="education-for-prospective-students.html">для
-                                    абітурієнтів</a></li>
-                                    <li><a id="adultlink" class="dropdown-item" href="education-for-adults.html">для
-                                    дорослих</a></li>`;
-            navMasterClasses = `<a class="nav-link menu-link" href="master.html">Майстер-класи</a>`;
-            navContacts = `<a class="nav-link menu-link" href="#contacts">Контакти</a>`;
-            mobLangSelect = `<a class="lang-select" href=${hrefLangUA()}>UA</a>
-                                                <div class="vertical"></div>
-                                            <a href=${hrefLangEN()}>EN</a>`;
-            navOrderLesson = "Замовити урок";
+
         }
     }
 
     header.innerHTML = `<nav class="navbar navbar-expand-lg bg-body-tertiary menu">
-                            <div class="container-fluid header-menu">
-                                <a href=${logoLink} title="">
-                                    <div class="header-logo"></div>
-                                </a>
-                                <div class="mob-lang-select">
-                                    ${mobLangSelect}
-                                </div>
-                                <div class="header-burger navbar-toggler" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation"><span></span></div>
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav me-auto mb-2 mb-lg-0 menu-list">
-                                    <div></div>
-                                    <li class="nav-item dropdown">
-                                        <span class="nav-link dropdown-toggle menu-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        ${navAboutUs}</ul>
-                                    </li>
-                                    <li class="nav-item dropdown" id="navstudy">
-                                        <span id="studylink" class="nav-link dropdown-toggle menu-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        ${navTeaching}</ul>
-                                    </li>
-                                    <li class="nav-item" id="navmaster">${navMasterClasses}</li>
-                                    <li class="nav-item" id="licontact">${navContacts}</li>
-                                    <li class="nav-item dropdown" id="navlang">
-                                        <span class="nav-link dropdown-toggle menu-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        ${navLangSelect}</ul>
-                                    </li>
-                                </ul>
-                            <div class="flash-btn"><div class="menu-flash"></div>
-                            <button class="header-btn popup-link" href="#popup">${navOrderLesson}</button>
-                            </div>
-                            <div class="header-icon-social">
-                            <a href="https://www.facebook.com/profile.php?id=100086510772962" class="social-link icon" title="facebook" target="_blank">
-                        <svg    svg class="icon">
-                                    <use xlink:href="src/icons/sprite.svg#facebook"></use>
-                                </svg>
-                            </a>
-                        <a href="https://www.instagram.com/art_studioleonardo/" class="social-link icon" title="instagram" target="_blank">
-                            <svg class="icon">
-                                <use xlink:href="src/icons/sprite.svg#instagram"></use>
-                            </svg>
-                        </a>
-                        </div></div></div>
-                    </nav>`;
+    <div class="container-fluid header-menu">
+        <a href="index.html" title="">
+            <div class="header-logo"></div>
+        </a>
+        <div class="mob-lang-select">
+            <a class="lang-select" href="/">UA</a>
+            <div class="vertical"></div>
+            <a href="/index-en.html">EN</a>
+        </div>
+        <div class="header-burger navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation"><span></span></div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 menu-list">
+                <div></div>
+                <li class="nav-item" id="about-link"><a class="nav-link menu-link" href="master.html">${aboutus}</a></li>
+                <li class="nav-item" id="lern-link"><a class="nav-link menu-link" href="education.html"
+                        target="_blank">${navlern}</a></li>
+                <li class="nav-item" id="project-link"><a class="nav-link menu-link" href="customers.html"
+                        target="_blank">Замовити проект</a></li>
+                <li class="nav-item" id="contact-link"><a class="nav-link menu-link"
+                        href="#contacts">Контакти</a></li>
+
+
+                <li class="nav-item dropdown" id="navlang">
+                    <span class="nav-link dropdown-toggle menu-link" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        UA</span>
+                    <ul class="dropdown-menu lang">
+                        <li><a class="dropdown-item lang" href="/">UA</a></li>
+                        <li><a class="dropdown-item lang" href="/index-en.html">EN</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="header-icon-social">
+                <a href="#" class="social-link icon" title="facebook" target="_blank">
+                    <svg svg="" class="icon">
+                        <use xlink:href="img/sprite.svg#facebook"></use>
+                    </svg>
+                </a>
+                <a href="#" class="social-link icon" title="instagram" target="_blank">
+                    <svg class="icon">
+                        <use xlink:href="img/sprite.svg#instagram"></use>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>`;
 }
 
 //variables ScrolUp btn
